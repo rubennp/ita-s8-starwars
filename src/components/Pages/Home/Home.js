cb1a993ee4660dc520d90500779cbe1910ee8837
Rubèn Nieto <ruben.np@me.com>
2021-08-01 22:20:12 +0200

    some changes on Home styling

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
+// import Droids from '../../../assets/droids.jpeg';
 import BB8 from '../../../assets/bb8.jpg';
 
 const Home = ({history}) => (
-    <Hero class="container col-xxl-8 px-4 py-5">
+    <Hero className="container col-xxl-8 px-4 py-5">
         <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
             <div class="col-10 col-sm-8 col-lg-6">
-                <img src={BB8} class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
+                <img src={BB8} class="d-block mx-lg-auto img-fluid rounded" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
             </div>
             <div class="col-lg-6">
                 <h1 class="display-5 fw-bold lh-1 mb-3">This is, <span>yes</span>, the web you were looking for!</h1>
@@ -29,7 +30,7 @@ const Hero = styled.div`
     box-shadow: 0 0 20px white;
 
     img {
-        box-shadow: 5px 5px 10px rgba(0, 0, 0, .5)
+        box-shadow: 5px 5px 25px rgba(0, 0, 0, .25)
     }
 
    span {
